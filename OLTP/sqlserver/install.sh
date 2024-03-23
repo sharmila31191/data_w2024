97ad17224d78554d170496051ca98ce48c8a9a15
sharmila31191 <164655427+sharmila31191@users.noreply.github.com>
2024-03-24 01:47:13 +0800

    Create install.sh

---
+++ OLTP/sqlserver/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Set SQL Server command-line tool (sqlcmd) environment variables
+export SA_PASSWORD=YourStrongPassw0rd
+export SERVER=localhost
+
+echo $SA_PASSWORD
+echo $SERVER
+# Create databases
+/opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -Q "CREATE DATABASE Adventureworks;"
+/opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -Q "CREATE DATABASE AdventureworksDW;"
+/opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -Q "CREATE DATABASE AdventureworksDWDemo;"
